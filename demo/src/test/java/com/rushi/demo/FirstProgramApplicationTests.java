d722adbdfdd55b1a758dab1643be58bd466131a8
RushiMore <rushi0957@gmail.com>
2026-01-12 21:39:42 +0530

    Day 1: Spring Boot Hello World API

---
+++ demo/src/test/java/com/rushi/demo/FirstProgramApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rushi.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstProgramApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
