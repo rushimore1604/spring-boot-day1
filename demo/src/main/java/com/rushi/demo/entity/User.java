35b9c3941390f2713be6804af23166f5008c61bc
RushiMore <rushi0957@gmail.com>
2026-01-30 21:45:16 +0530

    Fix user search query and login issue, add username/password authentication

--- demo/src/main/java/com/rushi/demo/entity/User.java
+++ demo/src/main/java/com/rushi/demo/entity/User.java
@@ -10,21 +10,35 @@ public class User {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    private String name;
+
+    private Integer age;
+
+    // If you are using login
     private String username;
 
     private String password;
 
-    private int age;
-
-    public User() {
-    }
+    // getters and setters
 
     public Long getId() {
         return id;
     }
 
-    public void setId(Long id) {
-        this.id = id;
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
     }
 
     public String getUsername() {
@@ -41,13 +55,5 @@ public class User {
 
     public void setPassword(String password) {
         this.password = password;
-    }
-
-    public int getAge() {
-        return age;
-    }
-
-    public void setAge(int age) {
-        this.age = age;
     }
 }
